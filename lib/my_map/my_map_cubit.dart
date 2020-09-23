24b9b5d416bd17484723a1c4696d9b8dc0db4d8f
Miles Krell <noreply@mileskrell.com>
2020-09-23 12:31:18 -0400

    Rename "MapCubit" to "MyMapCubit"

--- lib/my_map/my_map_cubit.dart
+++ lib/my_map/my_map_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:bloc/bloc.dart';
 
-class MapCubit extends Cubit<List<dynamic>> {
-  MapCubit() : super(<dynamic>[]);
+class MyMapCubit extends Cubit<List<dynamic>> {
+  MyMapCubit() : super(<dynamic>[]);
 
   void setRoutes(List<dynamic> newRoutes) => emit(newRoutes);
 }
